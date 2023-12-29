ef9fc54e024bd7936c0ad4658fb20a25369f9c98
TheComamba <thecomamba@jojoheinze.de>
2023-12-29 22:08:21 +0100

    Adding more planets to the example.

--- src/model/example.rs
+++ src/model/example.rs
@@ -1,13 +1,6 @@
 use super::celestial_system::CelestialSystem;
 use crate::model::{celestial_body_data::CelestialBodyData, orbital_parameters::OrbitalParameters};
-use astro_utils::{
-    orbit_orientation::OrbitOrientation,
-    solar_system_data::{
-        EARTH_MASS, EARTH_RADIUS, EARTH_SEMI_MAJOR_AXIS, JUPITER_SEMI_MAJOR_AXIS, MOON_MASS,
-        MOON_RADIUS, MOON_SEMI_MAJOR_AXIS, SUN_MASS, SUN_RADIUS,
-    },
-    units::{angle::Angle, length::Length, mass::Mass},
-};
+use astro_utils::{orbit_orientation::OrbitOrientation, solar_system_data::*, units::angle::Angle};
 
 fn sun() -> CelestialBodyData {
     CelestialBodyData::new(
@@ -19,39 +12,183 @@ fn sun() -> CelestialBodyData {
     )
 }
 
+fn mercury() -> CelestialBodyData {
+    CelestialBodyData::new(
+        String::from("Mercury"),
+        MERCURY_MASS,
+        OrbitalParameters::new(
+            MERCURY_SEMI_MAJOR_AXIS,
+            MERCURY_ECCENTRICITY,
+            OrbitOrientation::new(
+                MERCURY_INCLINATION,
+                MERCURY_LONGITUDE_OF_ASCENDING_NODE,
+                MERCURY_ARGUMENT_OF_PERIAPSIS,
+            ),
+        ),
+        MERCURY_RADIUS,
+        MERCURY_ALBEDO,
+    )
+}
+
+fn venus() -> CelestialBodyData {
+    CelestialBodyData::new(
+        String::from("Venus"),
+        VENUS_MASS,
+        OrbitalParameters::new(
+            VENUS_SEMI_MAJOR_AXIS,
+            VENUS_ECCENTRICITY,
+            OrbitOrientation::new(
+                VENUS_INCLINATION,
+                VENUS_LONGITUDE_OF_ASCENDING_NODE,
+                VENUS_ARGUMENT_OF_PERIAPSIS,
+            ),
+        ),
+        VENUS_RADIUS,
+        VENUS_ALBEDO,
+    )
+}
+
 fn earth() -> CelestialBodyData {
     CelestialBodyData::new(
         String::from("Earth"),
         EARTH_MASS,
         OrbitalParameters::new(
             EARTH_SEMI_MAJOR_AXIS,
-            0.0167086,
+            EARTH_ECCENTRICITY,
             OrbitOrientation::new(
-                Angle::from_degrees(0.0),
-                Angle::from_degrees(-11.26064),
-                Angle::from_degrees(114.20783),
+                EARTH_INCLINATION,
+                EARTH_LONGITUDE_OF_ASCENDING_NODE,
+                EARTH_ARGUMENT_OF_PERIAPSIS,
             ),
         ),
         EARTH_RADIUS,
-        1.0,
+        EARTH_ALBEDO,
+    )
+}
+
+fn mars() -> CelestialBodyData {
+    CelestialBodyData::new(
+        String::from("Mars"),
+        MARS_MASS,
+        OrbitalParameters::new(
+            MARS_SEMI_MAJOR_AXIS,
+            MARS_ECCENTRICITY,
+            OrbitOrientation::new(
+                MARS_INCLINATION,
+                MARS_LONGITUDE_OF_ASCENDING_NODE,
+                MARS_ARGUMENT_OF_PERIAPSIS,
+            ),
+        ),
+        MARS_RADIUS,
+        MARS_ALBEDO,
+    )
+}
+
+fn ceres() -> CelestialBodyData {
+    CelestialBodyData::new(
+        String::from("Ceres"),
+        CERES_MASS,
+        OrbitalParameters::new(
+            CERES_SEMI_MAJOR_AXIS,
+            CERES_ECCENTRICITY,
+            OrbitOrientation::new(
+                CERES_INCLINATION,
+                CERES_LONGITUDE_OF_ASCENDING_NODE,
+                CERES_ARGUMENT_OF_PERIAPSIS,
+            ),
+        ),
+        CERES_RADIUS,
+        CERES_ALBEDO,
     )
 }
 
 fn jupiter() -> CelestialBodyData {
     CelestialBodyData::new(
         String::from("Jupiter"),
-        Mass::from_jupiter_masses(1.0),
+        JUPITER_MASS,
         OrbitalParameters::new(
             JUPITER_SEMI_MAJOR_AXIS,
-            0.0489,
+            JUPITER_ECCENTRICITY,
             OrbitOrientation::new(
-                Angle::from_degrees(1.303),
-                Angle::from_degrees(100.464),
-                Angle::from_degrees(273.867),
+                JUPITER_INCLINATION,
+                JUPITER_LONGITUDE_OF_ASCENDING_NODE,
+                JUPITER_ARGUMENT_OF_PERIAPSIS,
             ),
         ),
-        Length::from_earth_radii(10.97),
-        1.0,
+        JUPITER_RADIUS,
+        JUPITER_ALBEDO,
+    )
+}
+
+fn saturn() -> CelestialBodyData {
+    CelestialBodyData::new(
+        String::from("Saturn"),
+        SATURN_MASS,
+        OrbitalParameters::new(
+            SATURN_SEMI_MAJOR_AXIS,
+            SATURN_ECCENTRICITY,
+            OrbitOrientation::new(
+                SATURN_INCLINATION,
+                SATURN_LONGITUDE_OF_ASCENDING_NODE,
+                SATURN_ARGUMENT_OF_PERIAPSIS,
+            ),
+        ),
+        SATURN_RADIUS,
+        SATURN_ALBEDO,
+    )
+}
+
+fn uranus() -> CelestialBodyData {
+    CelestialBodyData::new(
+        String::from("Uranus"),
+        URANUS_MASS,
+        OrbitalParameters::new(
+            URANUS_SEMI_MAJOR_AXIS,
+            URANUS_ECCENTRICITY,
+            OrbitOrientation::new(
+                URANUS_INCLINATION,
+                URANUS_LONGITUDE_OF_ASCENDING_NODE,
+                URANUS_ARGUMENT_OF_PERIAPSIS,
+            ),
+        ),
+        URANUS_RADIUS,
+        URANUS_ALBEDO,
+    )
+}
+
+fn neptune() -> CelestialBodyData {
+    CelestialBodyData::new(
+        String::from("Neptune"),
+        NEPTUNE_MASS,
+        OrbitalParameters::new(
+            NEPTUNE_SEMI_MAJOR_AXIS,
+            NEPTUNE_ECCENTRICITY,
+            OrbitOrientation::new(
+                NEPTUNE_INCLINATION,
+                NEPTUNE_LONGITUDE_OF_ASCENDING_NODE,
+                NEPTUNE_ARGUMENT_OF_PERIAPSIS,
+            ),
+        ),
+        NEPTUNE_RADIUS,
+        NEPTUNE_ALBEDO,
+    )
+}
+
+fn pluto() -> CelestialBodyData {
+    CelestialBodyData::new(
+        String::from("Pluto"),
+        PLUTO_MASS,
+        OrbitalParameters::new(
+            PLUTO_SEMI_MAJOR_AXIS,
+            PLUTO_ECCENTRICITY,
+            OrbitOrientation::new(
+                PLUTO_INCLINATION,
+                PLUTO_LONGITUDE_OF_ASCENDING_NODE,
+                PLUTO_ARGUMENT_OF_PERIAPSIS,
+            ),
+        ),
+        PLUTO_RADIUS,
+        PLUTO_ALBEDO,
     )
 }
 
@@ -74,7 +211,15 @@ fn _moon() -> CelestialBodyData {
 }
 pub(crate) fn solar_system() -> CelestialSystem {
     let mut system = CelestialSystem::new(sun());
+    system.add_planet(mercury());
+    system.add_planet(venus());
     system.add_planet(earth());
+    system.add_planet(mars());
     system.add_planet(jupiter());
+    system.add_planet(saturn());
+    system.add_planet(uranus());
+    system.add_planet(neptune());
+    system.add_planet(pluto());
+    system.add_planet(ceres());
     system
 }
